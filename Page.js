1d20631d9beb5d3b45bace8b25087b7205a45f70
Eunbyul Na <51739602+EunbyulNa@users.noreply.github.com>
2019-07-25 18:16:09 +0100

    This is the first version of my webpage for FAC

---
+++ Page.js
@@ -0,0 +1,11 @@
+function oepnSlideMenu() {
+  document.getElementById('menu').style.width = '250px';
+  document.getElementById('content').style.marginRight= '250px';
+
+}
+
+function closeSlideMenu() {
+  document.getElementById('menu').style.width = '0px';
+  document.getElementById('content').style.marginRight= '0px';
+
+}
